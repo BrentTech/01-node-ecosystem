de2366c600eeb038944b722135d653fca6b27a83
Brent Woodward <Brent.Woodward.1@gmail.com>
2018-12-18 08:43:54 -0800

    ReadME generated

--- index.js
+++ index.js
@@ -6,3 +6,5 @@ const math = require('./lib/arithmetic.js');
 console.log(greet('JOHN'));
 console.log(math.add(1,3)); // 4
 console.log(math.subtract(1,3)); // -2
+console.log(math.multiply(5,5)); // 25
+console.log(math.divide(10,2)); // 5
